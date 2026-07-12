9cbfbc704ad45dfe64461315dc55c0fd420e5c51
Ebo1996 <ebisaberhanu1996@gmail.com>
2026-07-12 17:12:50 +0300

    update dbconnection.php for production deployment

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include "setup/dbconnection.php";
 $sql = "SELECT * FROM feedback ";
 $stmt = $conn->prepare($sql);
--- setup/dbconnection.php
+++ setup/dbconnection.php
@@ -1,8 +1,8 @@
 <?php
-$hostName = "localhost";
-$username = "root";
-$password = "";
-$database = "birth_certificate_database";
+$hostName = "sql305.infinityfree.com";
+$username = "if0_42392408";
+$password = "XWC2i6RYmJciclb";
+$database = "if0_42392408_obcsdb";
 
 $conn = new mysqli($hostName, $username, $password, $database);
 if ($conn->connect_error) {
